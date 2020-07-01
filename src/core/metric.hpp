b40c1d72c1bce3abd12f9b82400e01db35a9d6e9
Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
2020-07-01 12:13:11 +0200

    Initial commit of MonitoringService application
    This is initial commit of the application, it consists of most of the
    features for OpenBMC Telemetry solution design implemented.
    See README.md for details
    
    Testing:
    - tested with upstream (in the review) Redfish frontend
    -- creating MetricReportDefinitions
    -- polling MetricReports
    -- both OnRequest and Periodic modes verified (OnChange TBD)
    
    Change-Id: I3819c5cae48a622b05f11b3f6d347abfe07a452f
    Signed-off-by: Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>

---
+++ src/core/metric.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+#include "interfaces/metric.hpp"
+#include "utils/compare_underlying_objects.hpp"
+
+#include <exception>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace core
+{
+
+class Metric :
+    public interfaces::Metric,
+    public std::enable_shared_from_this<Metric>
+{
+  public:
+    Metric(const std::string& metricId, const std::string& metadata,
+           const std::vector<std::shared_ptr<interfaces::Sensor>>&& sensors,
+           const OperationType type) :
+        metricId_(metricId),
+        metadata_(metadata), sensors_(std::move(sensors)), type_(type)
+    {
+        if (OperationType::single != type || sensors.size() != 1)
+        {
+            // TODO: Specialize exceptions
+            throw std::runtime_error(
+                "Currently only single-sensor metric is supported");
+        }
+    }
+
+    bool operator==(const interfaces::Metric& other) const override
+    {
+        return type() == other.type() && id() == other.id() &&
+               metadata() == other.metadata() &&
+               std::equal(sensors().begin(), sensors().end(),
+                          other.sensors().begin(), other.sensors().end(),
+                          utils::CompareUnderlyingObjects()) &&
+               std::equal(readings().begin(), readings().end(),
+                          other.readings().begin(), other.readings().end(),
+                          utils::CompareUnderlyingObjects());
+    }
+
+    OperationType type() const override
+    {
+        return type_;
+    }
+
+    std::string_view id() const override
+    {
+        return metricId_;
+    }
+
+    std::string_view metadata() const override
+    {
+        return metadata_;
+    }
+
+    const std::vector<std::shared_ptr<interfaces::Sensor>>&
+        sensors() const override
+    {
+        return sensors_;
+    }
+
+    boost::beast::span<const std::optional<Reading>> readings() const override
+    {
+        return {readings_.data(), readings_.size()};
+    }
+
+    void async_read(ReadingsUpdated&& callback) override
+    {
+        // Right now only passthrough, later - operation after all complete
+        auto sharedCallback = std::make_shared<std::decay_t<ReadingsUpdated>>(
+            std::move(callback));
+
+        // TODO: Implement for multiple sensors (perform operation)
+        sensors_[0]->async_read(
+            [me = shared_from_this(), sharedCallback](
+                const boost::system::error_code& e, const double value) {
+                if (!e)
+                {
+                    me->readings_[0] = Reading{value, std::time(0)};
+                }
+
+                // TODO: if(thisIsLastReading)
+                (*sharedCallback)();
+            });
+    }
+
+    void registerForUpdates() override
+    {
+        sensors_[0]->registerForUpdates(
+            [weakSelf = weak_from_this()](const boost::system::error_code& e,
+                                          const double value) {
+                if (!e)
+                {
+                    if (auto self = weakSelf.lock())
+                    {
+                        self->readings_[0] = Reading{value, std::time(0)};
+                    }
+                }
+            });
+    }
+
+  private:
+    const std::string metricId_;
+    const std::string metadata_;
+    const std::vector<std::shared_ptr<interfaces::Sensor>> sensors_;
+    std::vector<std::optional<Reading>> readings_{sensors_.size()};
+    const OperationType type_;
+
+    Metric(const Metric&) = delete;
+    Metric& operator=(const Metric&) = delete;
+};
+} // namespace core
