b310c80a23f36d7fd12f549a916834ad4cf2274a
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-18 10:17:16 +0200

    Initial implementation of MonitoringService
    Implemented main application of MonitoringService.
    Added ReportManager interface. Added MaxReports and
    PollRateResolution properties to ReportManager interface.
    Implemented simple logger.
    
    Tested:
     - Built without Yocto and ran on x86 platform with success
     - Added monitoring-service to romulus image using
       recipe-phosphor/telemetry from meta-phosphor repository
     - Started as service in romulus image in QEMU with success
     - Verified that all added properties are present in dbus
    
    Change-Id: I26af7a19b1f9cac32e9e9da65523d72a36e13855
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

---
+++ cmake/flags.cmake
@@ -0,0 +1,40 @@
+set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+    -Werror \
+    -Wall \
+    -Wextra \
+    -Wnon-virtual-dtor \
+    -Wold-style-cast \
+    -Wcast-align \
+    -Wunused \
+    -Woverloaded-virtual \
+    -Wpedantic \
+    -Wconversion \
+    -Wsign-conversion \
+    -Wnull-dereference \
+    -Wduplicated-cond \
+    -Wduplicated-branches \
+    -Wlogical-op \
+    -Wdouble-promotion \
+    -Wformat=2 \
+    -Wno-unused-parameter \
+    -Os \
+    -flto \
+    -fno-rtti \
+")
+
+# Security-oriented compilation flags
+set (SECURITY_FLAGS
+    "-fstack-protector-strong \
+    -fPIE \
+    -fPIC \
+    -D_FORTIFY_SOURCE=2 \
+    -Wformat \
+    -Wformat-security \
+")
+
+set (CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} \
+    ${SECURITY_FLAGS}")
+set (CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} \
+    ${SECURITY_FLAGS}")
+set (CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS_MINSIZEREL} \
+    ${SECURITY_FLAGS}")
