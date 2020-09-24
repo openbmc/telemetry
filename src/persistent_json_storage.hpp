173334882afce1e621e85b51ef4afab4031d098c
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-24 13:42:04 +0200

    Added PersistentJsonStorageClass
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: Ib496e6782e849d910fe37c02c355047afda5c79c

---
+++ src/persistent_json_storage.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "interfaces/json_storage.hpp"
+
+class PersistentJsonStorage : public interfaces::JsonStorage
+{
+  public:
+    explicit PersistentJsonStorage(const DirectoryPath& directory);
+
+    void store(const FilePath& subPath, const nlohmann::json& data) override;
+    bool remove(const FilePath& subPath) override;
+    std::optional<nlohmann::json> load(const FilePath& subPath) const override;
+    std::vector<FilePath>
+        list(const DirectoryPath& subDirectory) const override;
+
+  private:
+    DirectoryPath directory;
+
+    static std::filesystem::path join(const std::filesystem::path&,
+                                      const std::filesystem::path&);
+    static void limitPermissions(const std::filesystem::path& path);
+};
