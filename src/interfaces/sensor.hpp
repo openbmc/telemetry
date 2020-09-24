7f06f613fff4eb4d84f74ce9d0dc5c68211f6ab1
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-24 13:42:10 +0200

    Created sensor cache class
    Created sensor cache and sensor interface that needs to be
    implemented by sensors.
    
    Tested:
    - Sensors created by sensor cache are stored and reused if
      there is try to access same sensor multiple times.
    - All other units tests are passing
    
    Change-Id: I552b2016bca4688e1b2a223297587826af256b54
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

---
+++ src/interfaces/sensor.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <ostream>
+#include <string>
+#include <string_view>
+#include <tuple>
+
+namespace interfaces
+{
+
+class Sensor
+{
+  public:
+    struct Id
+    {
+        Id(std::string_view type, std::string_view service,
+           std::string_view path) :
+            type(type),
+            service(service), path(path)
+        {}
+
+        std::string type;
+        std::string service;
+        std::string path;
+
+        bool operator<(const Id& other) const
+        {
+            return std::tie(type, service, path) <
+                   std::tie(other.type, other.service, other.path);
+        }
+    };
+
+    virtual ~Sensor() = default;
+
+    virtual Id id() const = 0;
+};
+
+} // namespace interfaces
