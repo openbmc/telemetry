0cd2d7f5106461ea7a0722a7d75957b93ab30d9c
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-24 13:42:04 +0200

    added PersistentJsonStorageClass
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: Ib496e6782e849d910fe37c02c355047afda5c79c

---
+++ src/persistent_json_storage.cpp
@@ -0,0 +1,144 @@
+#include "persistent_json_storage.hpp"
+
+#include "utils/api_error.hpp"
+#include "utils/log.hpp"
+
+#include <fstream>
+
+PersistentJsonStorage::PersistentJsonStorage(const DirectoryPath& directory) :
+    directory(directory)
+{}
+
+void PersistentJsonStorage::store(const FilePath& filePath,
+                                  const nlohmann::json& data)
+{
+    try
+    {
+        const auto path = join(directory, filePath);
+        std::error_code ec;
+
+        LOG_DEBUG << "Writing file at: " << path;
+
+        std::filesystem::create_directories(path.parent_path(), ec);
+        if (ec)
+        {
+            apiError(std::errc::io_error,
+                     "Unable to create directory for file: " + path.string() +
+                         ", ec=" + std::to_string(ec.value()) + ": " +
+                         ec.message());
+        }
+
+        std::ofstream file(path);
+        file << data;
+        if (!file)
+        {
+            apiError(std::errc::io_error,
+                     "Unable to create file: " + path.string());
+        }
+
+        limitPermissions(path.parent_path());
+        limitPermissions(path);
+    }
+    catch (...)
+    {
+        remove(filePath);
+        throw;
+    }
+}
+
+bool PersistentJsonStorage::remove(const FilePath& filePath)
+{
+    const auto path = join(directory, filePath);
+    std::error_code ec;
+
+    auto removed = std::filesystem::remove(path, ec);
+    if (!removed)
+    {
+        LOG_ERROR << "Unable to remove file: " << path << ", ec=" << ec.value()
+                  << ": " << ec.message();
+        return false;
+    }
+
+    /* removes directory only if it is empty */
+    std::filesystem::remove(path.parent_path(), ec);
+
+    return true;
+}
+
+std::optional<nlohmann::json>
+    PersistentJsonStorage::load(const FilePath& filePath) const
+{
+    const auto path = join(directory, filePath);
+    if (!std::filesystem::exists(path))
+    {
+        return std::nullopt;
+    }
+
+    nlohmann::json result;
+
+    try
+    {
+        std::ifstream file(path);
+        file >> result;
+    }
+    catch (const std::exception& e)
+    {
+        LOG_ERROR << e.what();
+        return std::nullopt;
+    }
+
+    return result;
+}
+
+std::vector<interfaces::JsonStorage::FilePath>
+    PersistentJsonStorage::list(const DirectoryPath& subDirectory) const
+{
+    auto result = std::vector<FilePath>();
+    const auto path = join(directory, subDirectory);
+
+    if (!std::filesystem::exists(path))
+    {
+        return result;
+    }
+
+    for (const auto& p : std::filesystem::directory_iterator(path))
+    {
+        if (std::filesystem::is_directory(p.path()))
+        {
+
+            for (auto& item : list(DirectoryPath(p.path())))
+            {
+                result.emplace_back(std::move(item));
+            }
+        }
+        else
+        {
+            const auto item = std::filesystem::relative(
+                p.path().parent_path(), std::filesystem::path{directory});
+
+            if (std::find(result.begin(), result.end(), item) == result.end())
+            {
+                result.emplace_back(item);
+            }
+        }
+    }
+
+    return result;
+}
+
+std::filesystem::path
+    PersistentJsonStorage::join(const std::filesystem::path& left,
+                                const std::filesystem::path& right)
+{
+    return left / right;
+}
+
+void PersistentJsonStorage::limitPermissions(const std::filesystem::path& path)
+{
+    constexpr auto filePerms = std::filesystem::perms::owner_read |
+                               std::filesystem::perms::owner_write;
+    constexpr auto dirPerms = filePerms | std::filesystem::perms::owner_exec;
+    std::filesystem::permissions(
+        path, std::filesystem::is_directory(path) ? dirPerms : filePerms,
+        std::filesystem::perm_options::replace);
+}
---
+++ tests/src/mocks/json_storage.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "interfaces/json_storage.hpp"
+
+#include <gmock/gmock.h>
+
+class StorageMock : public interfaces::JsonStorage
+{
+  public:
+    ~StorageMock();
+
+    MOCK_METHOD2(store, void(const FilePath&, const nlohmann::json&));
+    MOCK_METHOD1(remove, bool(const FilePath&));
+    MOCK_CONST_METHOD1(load, std::optional<nlohmann::json>(const FilePath&));
+    MOCK_CONST_METHOD1(list, std::vector<FilePath>(const DirectoryPath&));
+};
