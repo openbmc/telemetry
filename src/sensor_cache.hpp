20d301f11eca6b5e62a79b1f06c768c761af12b8
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-24 13:42:10 +0200

    created sensor cache class
    Change-Id: I552b2016bca4688e1b2a223297587826af256b54
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

---
+++ src/sensor_cache.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "interfaces/sensor.hpp"
+
+#include <boost/container/flat_map.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <iostream>
+#include <memory>
+#include <string_view>
+
+class SensorCache
+{
+    struct SensorIdCompare
+    {
+        bool operator()(const interfaces::Sensor::Id& left,
+                        const interfaces::Sensor::Id& right) const;
+    };
+
+  public:
+    template <class SensorType, class... Args>
+    std::shared_ptr<SensorType> makeSensor(std::string_view service,
+                                           std::string_view path,
+                                           Args&&... args)
+    {
+        cleanupExpiredSensors();
+
+        auto id = SensorType::makeId(service, path);
+        auto it = sensors.find(id);
+
+        if (it == sensors.end())
+        {
+            auto sensor = std::make_shared<SensorType>(
+                std::move(id), std::forward<Args>(args)...);
+
+            auto [kt, inserted] = sensors.insert(
+                SensorsContainer::value_type(sensor->id(), std::move(sensor)));
+
+            return std::static_pointer_cast<SensorType>(kt->second.lock());
+        }
+
+        return std::static_pointer_cast<SensorType>(it->second.lock());
+    }
+
+  private:
+    using SensorsContainer =
+        boost::container::flat_map<interfaces::Sensor::Id,
+                                   std::weak_ptr<interfaces::Sensor>,
+                                   SensorIdCompare>;
+
+    SensorsContainer sensors;
+
+    std::optional<SensorsContainer::iterator> findExpiredSensor();
+    void cleanupExpiredSensors();
+};
