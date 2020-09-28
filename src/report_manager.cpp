3a757bb4162937f484ef22a4b8d218de1f83c337
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-09-28 16:38:57 +0200

    Add Report interface to dbus
    Change-Id: Ib20cfda1f0e27ef20b60e66d52c3d11f31f61a96
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>

--- src/report_manager.cpp
+++ src/report_manager.cpp
@@ -10,7 +10,8 @@ constexpr const char* reportManagerPath =
     "/xyz/openbmc_project/MonitoringService/Reports";
 
 ReportManager::ReportManager(
-    const std::shared_ptr<sdbusplus::asio::object_server>& objServer)
+    const std::shared_ptr<sdbusplus::asio::object_server>& objServer) :
+    reports{}
 {
     reportManagerIntf = utils::DbusInterface(
         objServer, reportManagerPath, reportManagerIfaceName,
@@ -25,5 +26,41 @@ ReportManager::ReportManager(
                 [](const auto&) -> uint64_t {
                     return pollRateResolution.count();
                 });
+
+            interface->register_method(
+                "AddReport",
+                [this, &objServer](const std::string& name,
+                                   const std::string& domain,
+                                   const std::string& reportingType,
+                                   const std::vector<std::string>& reportAction,
+                                   const uint64_t scanPeriod,
+                                   const ReadingParameters& metricParams) {
+                    if (!verifyScanPeriod(scanPeriod))
+                    {
+                        throw sdBusError(std::errc::invalid_argument,
+                                         "Invalid scanPeriod");
+                    }
+                    if (reports.size() >= maxReports)
+                    {
+                        throw sdBusError(std::errc::too_many_files_open,
+                                         "Reached maximal report count");
+                    }
+
+                    auto report = std::make_shared<Report>(
+                        objServer, domain, name,
+                        std::chrono::milliseconds{scanPeriod});
+                    reports.push_back(report);
+                    return report->path;
+                });
         });
+}
+
+bool ReportManager::verifyScanPeriod(const uint64_t scanPeriod)
+{
+    if (scanPeriod < pollRateResolution.count() ||
+        scanPeriod % pollRateResolution.count() == 0)
+    {
+        return false;
+    }
+    return true;
 }
