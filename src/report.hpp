5575e9d510b54cc3dae57d5592303965b934386f
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-09-28 16:38:57 +0200

    Add Report interface to dbus
    Added AddReport method to ReportManager to allow dbus user
    to create new Report. Added Report object with Report and Delete
    interfaces.
    
    Tested:
     - Verified that telemetry service works in romulus
     - Added and removed report from ReportManager collection
    
    Change-Id: Ib20cfda1f0e27ef20b60e66d52c3d11f31f61a96
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>

---
+++ src/report.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <boost/asio/io_context.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <chrono>
+#include <memory>
+
+using Readings = std::tuple<
+    uint64_t,
+    std::vector<std::tuple<std::string, std::string, double, uint64_t>>>;
+using ReadingParameters =
+    std::vector<std::tuple<std::vector<sdbusplus::message::object_path>,
+                           std::string, std::string, std::string>>;
+
+class Report
+{
+  public:
+    Report(boost::asio::io_context& io_context,
+           const std::shared_ptr<sdbusplus::asio::object_server>& objServer,
+           const std::string& name, const std::string& reportingType,
+           const bool emitsReadingsSignal,
+           const bool logToMetricReportsCollection,
+           const std::chrono::milliseconds&& period,
+           const ReadingParameters& metricParams,
+           std::function<void(const Report&)> removeCallback);
+    ~Report();
+
+    std::string name;
+    std::string path;
+
+  private:
+    std::chrono::milliseconds interval;
+    std::shared_ptr<sdbusplus::asio::object_server> objServer;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> reportIntf;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> deleteIface;
+};
