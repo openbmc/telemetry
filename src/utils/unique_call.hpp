cac153036c106e98bf58c9e517dc7bbae1b47ef7
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-29 11:52:45 +0200

    Implemented sensor class
    Sensor class was introduced, it monitors
    xyz.openbmc_project.Sensor.Value, for change and notifies all
    listeners.
    
    Tested:
      - Unit tested with service stub that provides dbus interface
        xyz.openbmc_project.Sensor.Value
      - All changes are delivered to listeners
      - All other unit tests are passing
    
    Change-Id: I8c9d58cc986c1fe2a4d2386815d559814016efa6
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

---
+++ src/utils/unique_call.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <memory>
+#include <utility>
+
+namespace utils
+{
+
+class UniqueCall
+{
+  public:
+    struct Lock
+    {
+        explicit Lock(UniqueCall& unique) : unique(unique)
+        {
+            unique.value = true;
+        }
+
+        Lock(const Lock&) = delete;
+        Lock(Lock&& other) = delete;
+
+        Lock& operator=(const Lock&) = delete;
+        Lock& operator=(Lock&& other) = delete;
+
+        ~Lock()
+        {
+            unique.value = false;
+        }
+
+      private:
+        UniqueCall& unique;
+    };
+
+    template <class Functor, class... Args>
+    void operator()(Functor&& functor, Args&&... args)
+    {
+        if (value == false)
+        {
+            functor(std::make_shared<Lock>(*this), std::forward<Args>(args)...);
+        }
+    }
+
+  private:
+    bool value = false;
+};
+
+} // namespace utils
---
+++ tests/src/dbus_environment.hpp
@@ -0,0 +1,68 @@
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <future>
+#include <thread>
+
+#include <gmock/gmock.h>
+
+class DbusEnvironment : public ::testing::Environment
+{
+  public:
+    ~DbusEnvironment();
+
+    void SetUp() override;
+    void TearDown() override;
+    void teardown();
+
+    static boost::asio::io_context& getIoc();
+    static std::shared_ptr<sdbusplus::asio::connection> getBus();
+    static std::shared_ptr<sdbusplus::asio::object_server> getObjServer();
+    static const char* serviceName();
+    static bool synchronizeIoc(
+        std::chrono::milliseconds timeout = std::chrono::seconds(5));
+    static std::function<void()> setPromise(std::string_view name);
+    static bool waitForFuture(
+        std::string_view name,
+        std::chrono::milliseconds timeout = std::chrono::seconds(5));
+
+    template <class Functor>
+    static bool synchronizedPost(
+        Functor&& functor,
+        std::chrono::milliseconds timeout = std::chrono::seconds(5))
+    {
+        auto promise = std::make_unique<std::promise<bool>>();
+        auto future = promise->get_future();
+
+        boost::asio::post(
+            ioc, [promise = std::move(promise), functor = std::move(functor)] {
+                try
+                {
+                    functor();
+                    promise->set_value(true);
+                }
+                catch (const std::exception& e)
+                {
+                    promise->set_value(false);
+                }
+            });
+
+        if (future.wait_for(timeout) == std::future_status::ready)
+        {
+            return future.get();
+        }
+
+        return false;
+    }
+
+  private:
+    static std::future<bool> getFuture(std::string_view name);
+
+    static boost::asio::io_context ioc;
+    static std::shared_ptr<sdbusplus::asio::connection> bus;
+    static std::shared_ptr<sdbusplus::asio::object_server> objServer;
+    static std::mutex mutex;
+    static std::map<std::string, std::vector<std::future<bool>>> futures;
+
+    bool setUp = false;
+    std::thread iocThread;
+};
