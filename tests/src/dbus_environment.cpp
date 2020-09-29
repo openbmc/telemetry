dd90749466e978d8ced363cee82cd93e69bc3ea3
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-29 11:52:45 +0200

    Implemented sensor class
    Sensor class was introduced, it monitors
    xyz.openbmc_project.Sensor.Value, for change and notifies all
    listeners.
    
    Tested:
      - Unit tested with service stub that provides dbus interface
        xyz.openbmc_project.Sensor.Value
      - All changes are delivered to listeners
      - All other unit tests are passing
    
    Change-Id: I8c9d58cc986c1fe2a4d2386815d559814016efa6
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

---
+++ tests/src/dbus_environment.cpp
@@ -0,0 +1,136 @@
+#include "dbus_environment.hpp"
+
+#include <future>
+#include <thread>
+
+DbusEnvironment::~DbusEnvironment()
+{
+    teardown();
+}
+
+void DbusEnvironment::SetUp()
+{
+    if (setUp == false)
+    {
+        setUp = true;
+
+        bus = std::make_shared<sdbusplus::asio::connection>(ioc);
+        bus->request_name(serviceName());
+    }
+
+    objServer = std::make_unique<sdbusplus::asio::object_server>(bus);
+
+    iocThread = std::thread([this] { ioc.run(); });
+}
+
+void DbusEnvironment::TearDown()
+{
+    ioc.stop();
+    iocThread.join();
+    ioc.restart();
+
+    objServer = nullptr;
+}
+
+void DbusEnvironment::teardown()
+{
+    if (setUp == true)
+    {
+        setUp = false;
+
+        bus = nullptr;
+    }
+}
+
+boost::asio::io_context& DbusEnvironment::getIoc()
+{
+    return ioc;
+}
+
+std::shared_ptr<sdbusplus::asio::connection> DbusEnvironment::getBus()
+{
+    return bus;
+}
+
+std::shared_ptr<sdbusplus::asio::object_server> DbusEnvironment::getObjServer()
+{
+    return objServer;
+}
+
+const char* DbusEnvironment::serviceName()
+{
+    return "telemetry.ut";
+}
+
+bool DbusEnvironment::synchronizeIoc(std::chrono::milliseconds timeout)
+{
+    auto promise = std::make_unique<std::promise<bool>>();
+    auto future = promise->get_future();
+
+    boost::asio::post(
+        ioc, [promise = std::move(promise)] { promise->set_value(true); });
+
+    if (future.wait_for(timeout) == std::future_status::ready)
+    {
+        return future.get();
+    }
+
+    return false;
+}
+
+std::function<void()> DbusEnvironment::setPromise(std::string_view name)
+{
+    auto promise = std::make_shared<std::promise<bool>>();
+
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        futures[std::string(name)].emplace_back(promise->get_future());
+    }
+
+    return [p = std::move(promise)]() { p->set_value(true); };
+}
+
+bool DbusEnvironment::waitForFuture(std::string_view name,
+                                    std::chrono::milliseconds timeout)
+{
+    if (auto future = getFuture(name); future.valid())
+    {
+        try
+        {
+            if (future.wait_for(timeout) == std::future_status::ready)
+            {
+                return future.get();
+            }
+        }
+        catch (const std::future_error& e)
+        {
+            std::cerr << e.what() << "\n";
+        }
+    }
+
+    return false;
+}
+
+std::future<bool> DbusEnvironment::getFuture(std::string_view name)
+{
+    std::unique_lock<std::mutex> lock(mutex);
+
+    auto& data = futures[std::string(name)];
+    auto it = std::find_if(data.begin(), data.end(),
+                           [](const auto& f) { return f.valid(); });
+
+    if (it != data.end())
+    {
+        auto result = std::move(*it);
+        data.erase(it);
+        return result;
+    }
+
+    return {};
+}
+
+boost::asio::io_context DbusEnvironment::ioc;
+std::shared_ptr<sdbusplus::asio::connection> DbusEnvironment::bus;
+std::shared_ptr<sdbusplus::asio::object_server> DbusEnvironment::objServer;
+std::mutex DbusEnvironment::mutex;
+std::map<std::string, std::vector<std::future<bool>>> DbusEnvironment::futures;
