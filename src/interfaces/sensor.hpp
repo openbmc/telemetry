c9792be2594c4f9c210e0a87c32a477d1951c789
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-29 11:52:45 +0200

    implemented sensor class
    Change-Id: I8c9d58cc986c1fe2a4d2386815d559814016efa6
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

--- src/interfaces/sensor.hpp
+++ src/interfaces/sensor.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <chrono>
+#include <memory>
 #include <ostream>
 #include <string>
 #include <string_view>
 
 namespace interfaces
 {
+
+class SensorListener;
 
 class Sensor
 {
@@ -31,6 +35,10 @@ class Sensor
     virtual ~Sensor() = default;
 
     virtual Id id() const = 0;
+    virtual void async_read() = 0;
+    virtual void registerForUpdates(const std::weak_ptr<SensorListener>&) = 0;
+    virtual void schedule(std::chrono::milliseconds) = 0;
+    virtual void stop() = 0;
 };
 
 } // namespace interfaces
---
+++ tests/src/test_sensor.cpp
@@ -0,0 +1,163 @@
+#include "dbus_environment.hpp"
+#include "mocks/sensor_listener_mock.hpp"
+#include "sensor.hpp"
+#include "sensor_cache.hpp"
+#include "stubs/dbus_sensor_object.hpp"
+#include "utils/measure_elapsed_time.hpp"
+#include "utils/sensor_id_eq.hpp"
+
+#include <thread>
+
+#include <gmock/gmock.h>
+
+using namespace testing;
+using namespace std::chrono_literals;
+
+class TestSensor : public Test
+{
+  public:
+    void SetUp() override
+    {
+        sensorObject.value = 42.7;
+    }
+
+    void TearDown() override
+    {
+        DbusEnvironment::synchronizeIoc();
+    }
+
+    void completeAsyncRead()
+    {
+        std::shared_ptr<SensorListenerMock> mock =
+            std::make_shared<NiceMock<SensorListenerMock>>();
+        mock->delegateIgnoringArgs();
+
+        Expectation seq = EXPECT_CALL(*mock, sensorUpdated());
+        sut->registerForUpdates(mock);
+
+        EXPECT_CALL(*mock, sensorUpdated())
+            .After(seq)
+            .WillOnce(Invoke(DbusEnvironment::setPromise("async_read")));
+        sut->async_read();
+
+        DbusEnvironment::waitForFuture("async_read");
+    }
+
+    std::chrono::milliseconds notifiesInGivenIntervalAfterSchedule(
+        std::chrono::milliseconds interval);
+
+    stubs::DbusSensorObject sensorObject{DbusEnvironment::getIoc(),
+                                         DbusEnvironment::getBus(),
+                                         DbusEnvironment::getObjServer()};
+
+    SensorCache sensorCache;
+    std::shared_ptr<Sensor> sut = sensorCache.makeSensor<Sensor>(
+        DbusEnvironment::serviceName(), sensorObject.path(),
+        DbusEnvironment::getIoc(), DbusEnvironment::getBus());
+    std::shared_ptr<SensorListenerMock> listenerMock =
+        std::make_shared<StrictMock<SensorListenerMock>>();
+};
+
+TEST_F(TestSensor, createsCorretlyViaSensorCache)
+{
+    ASSERT_THAT(sut->id(),
+                sensorIdEq(Sensor::Id("Sensor", DbusEnvironment::serviceName(),
+                                      sensorObject.path())));
+}
+
+TEST_F(TestSensor, notifiesWithoutValueWhenThereIsNoValueDuringRegister)
+{
+    EXPECT_CALL(*listenerMock, sensorUpdated(Ref(*sut)));
+
+    sut->registerForUpdates(listenerMock);
+}
+
+TEST_F(TestSensor, notifiesWithValueWhenThereIsValueDuringRegister)
+{
+    completeAsyncRead();
+
+    EXPECT_CALL(*listenerMock, sensorUpdated(Ref(*sut), 42.7));
+    sut->registerForUpdates(listenerMock);
+}
+
+TEST_F(TestSensor, notifiesListenerWithValueWhenChangeOccurs)
+{
+    EXPECT_CALL(*listenerMock, sensorUpdated(Ref(*sut)));
+    sut->registerForUpdates(listenerMock);
+
+    EXPECT_CALL(*listenerMock, sensorUpdated(Ref(*sut), 42.7));
+    completeAsyncRead();
+}
+
+TEST_F(TestSensor, notifiesListenerWithoutValueWhenNoChangeOccurs)
+{
+    Sequence seq;
+    EXPECT_CALL(*listenerMock, sensorUpdated(Ref(*sut))).InSequence(seq);
+    EXPECT_CALL(*listenerMock, sensorUpdated(Ref(*sut), 42.7)).InSequence(seq);
+    EXPECT_CALL(*listenerMock, sensorUpdated(Ref(*sut))).InSequence(seq);
+
+    sut->registerForUpdates(listenerMock);
+
+    completeAsyncRead();
+    completeAsyncRead();
+}
+
+TEST_F(TestSensor, doesntNotifyExpiredListener)
+{
+    {
+        auto listener = std::make_shared<StrictMock<SensorListenerMock>>();
+        EXPECT_CALL(*listener, sensorUpdated(Ref(*sut)));
+        sut->registerForUpdates(listener);
+    }
+
+    completeAsyncRead();
+}
+
+std::chrono::milliseconds TestSensor::notifiesInGivenIntervalAfterSchedule(
+    std::chrono::milliseconds interval)
+{
+    auto listener = std::make_shared<NiceMock<SensorListenerMock>>();
+    listener->delegateIgnoringArgs();
+
+    sut->registerForUpdates(listener);
+
+    {
+        InSequence seq;
+        EXPECT_CALL(*listener, sensorUpdated()).Times(4);
+        EXPECT_CALL(*listener, sensorUpdated())
+            .WillOnce(Invoke(DbusEnvironment::setPromise("5th_async_read")));
+    }
+
+    sut->schedule(interval);
+
+    auto elapsed =
+        utils::measureElapsedTime<std::chrono::milliseconds>([interval] {
+            DbusEnvironment::waitForFuture("5th_async_read", interval * 10);
+        });
+
+    sut->stop();
+
+    return elapsed;
+}
+
+TEST_F(TestSensor, notifiesInGivenIntervalAfterSchedule)
+{
+    // When interval is too small update might be ommited (performance
+    // dependency). With interval big enough all updates should happen in given
+    // time frame.
+    for (auto interval : {10, 250, 750})
+    {
+        auto elapsed = notifiesInGivenIntervalAfterSchedule(
+                           std::chrono::milliseconds(interval))
+                           .count();
+        ASSERT_THAT(elapsed, Ge(interval * 5));
+
+        if (elapsed < interval * 6)
+        {
+            return;
+        }
+    }
+
+    ASSERT_THAT(notifiesInGivenIntervalAfterSchedule(1750ms).count(),
+                AllOf(Ge(1750 * 5), Lt(1750 * 6)));
+}
