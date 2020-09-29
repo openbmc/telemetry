4d60ba2518b35f395f498ee3004fc13e76b65b7d
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-29 11:52:45 +0200

    Implemented sensor class
    Sensor class was introduced, it monitors
    xyz.openbmc_project.Sensor.Value, for change and notifies all
    listeners.
    
    Tested:
      - Unit tested with service stub that provides dbus interface
        xyz.openbmc_project.Sensor.Value
      - All changes are delivered to listeners
      - All other unit tests are passing
    
    Change-Id: I8c9d58cc986c1fe2a4d2386815d559814016efa6
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

---
+++ tests/src/main.cpp
@@ -0,0 +1,16 @@
+#include "dbus_environment.hpp"
+
+#include <gmock/gmock.h>
+
+int main(int argc, char** argv)
+{
+    auto env = new DbusEnvironment;
+
+    testing::InitGoogleTest(&argc, argv);
+    testing::AddGlobalTestEnvironment(env);
+    auto ret = RUN_ALL_TESTS();
+
+    env->teardown();
+
+    return ret;
+}
