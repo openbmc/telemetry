65f1403c54c2673ecdc5fdf769fea2a8fa2dcb81
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-29 11:52:45 +0200

    implemented sensor class
    Change-Id: I8c9d58cc986c1fe2a4d2386815d559814016efa6
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

---
+++ tests/src/dbus_environment.hpp
@@ -0,0 +1,25 @@
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <thread>
+
+#include <gmock/gmock.h>
+
+class DbusEnvironment : public ::testing::Environment
+{
+  public:
+    void SetUp() override;
+    void TearDown() override;
+
+    static boost::asio::io_context& getIoc();
+    static std::shared_ptr<sdbusplus::asio::connection> getBus();
+    static std::shared_ptr<sdbusplus::asio::object_server> getObjServer();
+    static const char* serviceName();
+    static void sleep(std::chrono::milliseconds);
+
+  private:
+    static boost::asio::io_context ioc;
+    static std::shared_ptr<sdbusplus::asio::connection> bus;
+    static std::shared_ptr<sdbusplus::asio::object_server> objServer;
+
+    std::thread iocThread;
+};
