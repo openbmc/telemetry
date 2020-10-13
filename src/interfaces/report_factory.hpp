6d4cb9bcc2b473970e8c6dc5d961fdb28f810f27
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-10-13 09:07:45 +0200

    Add ReportManager and Report unit tests
    Introduced ReportFactory to seperate Report and ReportManager
    unit tests. Implemented mocks for Report, ReportManager and
    ReportFactory classes. Added tests for DBus Properties and Methods
    provided by telemetry service.
    
    Tested:
     - Ran unit-tests with success
    
    Change-Id: I1860e280d26ee4becc52de98dd65e5697d26b376
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>

---
+++ src/interfaces/report_factory.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "interfaces/report.hpp"
+#include "interfaces/report_manager.hpp"
+#include "interfaces/types.hpp"
+
+#include <chrono>
+#include <memory>
+
+class ReportManager;
+
+namespace interfaces
+{
+
+class ReportFactory
+{
+  public:
+    virtual ~ReportFactory() = default;
+
+    virtual std::unique_ptr<interfaces::Report>
+        make(const std::string& name, const std::string& reportingType,
+             bool emitsReadingsSignal, bool logToMetricReportsCollection,
+             std::chrono::milliseconds period,
+             const ReadingParameters& metricParams,
+             ReportManager& reportManager) const = 0;
+};
+
+} // namespace interfaces
