829126f3e586204cc342821783cc634a983be188
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-10-13 09:07:45 +0200

    Add ReportManager and Report unit tests
    Introduced ReportFactory to seperate Report and ReportManager
    unit tests. Implemented mocks for Report, ReportManager and
    ReportFactory classes. Added tests for DBus Properties and Methods
    provided by telemetry service.
    
    Tested:
     - Ran unit-tests with success
    
    Change-Id: I1860e280d26ee4becc52de98dd65e5697d26b376
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>

---
+++ src/report_factory.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "interfaces/report_factory.hpp"
+
+#include <boost/asio/io_context.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+class ReportFactory : public interfaces::ReportFactory
+{
+  public:
+    ReportFactory(
+        boost::asio::io_context& ioc,
+        const std::shared_ptr<sdbusplus::asio::object_server>& objServer);
+
+    std::unique_ptr<interfaces::Report>
+        make(const std::string& name, const std::string& reportingType,
+             bool emitsReadingsSignal, bool logToMetricReportsCollection,
+             std::chrono::milliseconds period,
+             const ReadingParameters& metricParams,
+             interfaces::ReportManager& reportManager) const override;
+
+  private:
+    boost::asio::io_context& ioc;
+    std::shared_ptr<sdbusplus::asio::object_server> objServer;
+};
