3f55dd298e9f18eae34ffc041fb2de214b7a8886
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-10-27 17:23:55 +0100

    Implement Report persistency
    Now Report properties are stored in non-volatile memory. It allows
    to restore Report after system restart. Persistency of a report is
    controlled by Persistency property in Report interface.
    
    Tested:
     - Passed unit tests
     - Verified that report is stored in /var/lib/telemetry dir
     - Verified that report is restored from storage after telemetry
       service start
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Change-Id: Iccfe21603eecffc4e174a4403f699b03de320db9

--- tests/src/params/report_params.hpp
+++ tests/src/params/report_params.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "interfaces/types.hpp"
+#include "report.hpp"
+#include "report_manager.hpp"
+#include "utils/transform.hpp"
+
 #include <chrono>
 #include <string>
 
@@ -28,7 +33,59 @@ class ReportParams final
         return reportingTypeProperty;
     }
 
+    bool emitReadingSignal() const
+    {
+        return emitReadingSignalProperty;
+    }
+
+    bool logToMetricReportCollection() const
+    {
+        return logToMetricReportCollectionProperty;
+    }
+
+    uint64_t interval() const
+    {
+        return intervalProperty;
+    }
+
+    const ReadingParameters& readingParameters() const
+    {
+        return readingParametersProperty;
+    }
+
+    nlohmann::json getJson() const
+    {
+        auto readingParametersJson =
+            utils::transform(readingParametersProperty, [](const auto& item) {
+                return ReadingParameterJson(&item);
+            });
+        return nlohmann::json{
+            {"Version", Report::reportVersion},
+            {"Name", reportNameProperty},
+            {"ReportingType", reportingTypeProperty},
+            {"EmitsReadingsUpdate", emitReadingSignalProperty},
+            {"LogToMetricReportsCollection",
+             logToMetricReportCollectionProperty},
+            {"Interval", intervalProperty},
+            {"ReadingParameters", readingParametersJson}};
+    }
+
   protected:
     std::string reportNameProperty = "TestReport";
     std::string reportingTypeProperty = "OnRequest";
+    bool emitReadingSignalProperty = true;
+    bool logToMetricReportCollectionProperty = true;
+    uint64_t intervalProperty = ReportManager::minInterval.count();
+    ReadingParameters readingParametersProperty = {
+        {{sdbusplus::message::object_path(
+             "/xyz/openbmc_project/sensors/power/p1")},
+         "SINGLE",
+         "MetricId1",
+         "Metadata1"},
+        {{sdbusplus::message::object_path(
+             "/xyz/openbmc_project/sensors/power/p2")},
+         "SINGLE",
+         "MetricId2",
+         "Metadata2"},
+    };
 };
