2a15684a8c4d81ebe8b6fe3e777ccfa82c40b517
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-10-27 17:23:55 +0100

    Implement Report persistency
    Now Report properties are stored in non-volatile memory. It allows
    to restore Report after system restart. Persistency of a report is
    controlled by Persistency property in Report interface.
    
    Tested:
     - Passed unit tests
     - Verified that report is stored in /var/lib/telemetry dir
     - Verified that report is restored from storage after telemetry
       service start
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>
    Change-Id: Iccfe21603eecffc4e174a4403f699b03de320db9

--- src/report_factory.cpp
+++ src/report_factory.cpp
@@ -2,23 +2,69 @@
 
 #include "report.hpp"
 
+#include <phosphor-logging/log.hpp>
+
+#include <stdexcept>
+
 ReportFactory::ReportFactory(
     boost::asio::io_context& ioc,
     const std::shared_ptr<sdbusplus::asio::object_server>& objServer) :
     ioc(ioc),
-    objServer(objServer)
+    objServer(objServer), reportStorage(interfaces::JsonStorage::DirectoryPath(
+                              "/var/lib/telemetry/Reports"))
 {}
 
 std::unique_ptr<interfaces::Report> ReportFactory::make(
     const std::string& name, const std::string& reportingType,
     bool emitsReadingsSignal, bool logToMetricReportsCollection,
     std::chrono::milliseconds period, const ReadingParameters& metricParams,
-    interfaces::ReportManager& reportManager) const
+    interfaces::ReportManager& reportManager)
 {
     std::vector<std::shared_ptr<interfaces::Metric>> metrics;
 
     return std::make_unique<Report>(
         ioc, objServer, name, reportingType, emitsReadingsSignal,
         logToMetricReportsCollection, period, metricParams, reportManager,
-        std::move(metrics));
+        std::move(metrics), reportStorage);
+}
+
+void ReportFactory::loadFromPersistent(interfaces::ReportManager& reportManager)
+{
+    std::vector<interfaces::JsonStorage::FilePath> paths = reportStorage.list();
+
+    for (const auto& path : paths)
+    {
+        std::optional<nlohmann::json> data = reportStorage.load(path);
+        try
+        {
+            size_t version = data->at("Version").get<size_t>();
+            if (version != Report::reportVersion)
+            {
+                throw std::logic_error("Invalid version");
+            }
+            std::string& name = data->at("Name").get_ref<std::string&>();
+            std::string& reportingType =
+                data->at("ReportingType").get_ref<std::string&>();
+            bool emitsReadingsSignal =
+                data->at("EmitsReadingsUpdate").get<bool>();
+            bool logToMetricReportsCollection =
+                data->at("LogToMetricReportsCollection").get<bool>();
+            uint64_t interval = data->at("Interval").get<uint64_t>();
+            ReadingParameters readingParameters;
+
+            reportManager.addReport(name, reportingType, emitsReadingsSignal,
+                                    logToMetricReportsCollection, interval,
+                                    readingParameters);
+        }
+        catch (std::exception& e)
+        {
+            phosphor::logging::log<phosphor::logging::level::ERR>(
+                "Failed to load report from storage",
+                phosphor::logging::entry(
+                    "filename=",
+                    static_cast<std::filesystem::path>(path).c_str()),
+                phosphor::logging::entry("msg=", e.what()));
+            reportStorage.remove(path);
+        }
+    }
 }
