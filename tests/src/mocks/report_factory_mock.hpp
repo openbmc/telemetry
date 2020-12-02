029aea2aa4e347132faefd1e5899b4ae2bdc3f81
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-12-02 09:27:28 +0000

    Review fixes for 'Created metric class'
    - Changed metric to_json to dumpConfiguration returning LabeledTuple
    - LabeledTuple can be created and assigned directly to json
    - LabeledTuple can be readed from json using json.get<LabeledTuple>
    - Added PrintTo for LabeledMetricParams, LabeledSensorParams
    - Added helper method expectMake to ReportFactoryMock
    - sensorPaths are serialized to tuple<service, path> instead of single
      field with service and path separated via ':'
    - Changed configuration version from 1 to 2
    
    Change-Id: I7c45fb584687172f88fd549a93329264793b0b8e
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

--- tests/src/mocks/report_factory_mock.hpp
+++ tests/src/mocks/report_factory_mock.hpp
@@ -2,6 +2,7 @@
 
 #include "interfaces/report_factory.hpp"
 #include "mocks/report_mock.hpp"
+#include "params/report_params.hpp"
 
 #include <gmock/gmock.h>
 
@@ -12,20 +13,73 @@ class ReportFactoryMock : public interfaces::ReportFactory
     {
         using namespace testing;
 
-        ON_CALL(*this, make)
+        ON_CALL(*this,
+                make(A<boost::asio::yield_context&>(), _, _, _, _, _, _, _, _))
             .WillByDefault(WithArgs<1>(Invoke([](const std::string& name) {
+                return std::make_unique<NiceMock<ReportMock>>(name);
+            })));
+        ON_CALL(*this, make(A<const std::string&>(), _, _, _, _, _, _, _, _))
+            .WillByDefault(WithArgs<0>(Invoke([](const std::string& name) {
                 return std::make_unique<NiceMock<ReportMock>>(name);
             })));
     }
 
-    MOCK_METHOD(
-        std::unique_ptr<interfaces::Report>, make,
-        (std::optional<std::reference_wrapper<boost::asio::yield_context>>,
-         const std::string& name, const std::string& reportingType,
-         bool emitsReadingsSignal, bool logToMetricReportsCollection,
-         std::chrono::milliseconds period,
-         const ReadingParameters& metricParams,
-         interfaces::ReportManager& reportManager,
-         interfaces::JsonStorage& reportStorage),
-        (const, override));
+    MOCK_METHOD(std::unique_ptr<interfaces::Report>, make,
+                (boost::asio::yield_context&, const std::string&,
+                 const std::string&, bool, bool, std::chrono::milliseconds,
+                 const ReadingParameters&, interfaces::ReportManager&,
+                 interfaces::JsonStorage&),
+                (const, override));
+    MOCK_METHOD(std::unique_ptr<interfaces::Report>, make,
+                (const std::string&, const std::string&, bool, bool,
+                 std::chrono::milliseconds, const ReadingParameters&,
+                 interfaces::ReportManager&, interfaces::JsonStorage&,
+                 std::vector<LabeledMetricParameters>),
+                (const, override));
+
+    auto& expectMake(
+        const testing::Matcher<boost::asio::yield_context&>& yield,
+        std::optional<std::reference_wrapper<const ReportParams>> paramsRef,
+        const testing::Matcher<interfaces::ReportManager&>& rm,
+        const testing::Matcher<interfaces::JsonStorage&>& js)
+    {
+        if (paramsRef)
+        {
+            const ReportParams& params = *paramsRef;
+            return EXPECT_CALL(*this, make(yield, params.reportName(),
+                                           params.reportingType(),
+                                           params.emitReadingUpdate(),
+                                           params.logToMetricReportCollection(),
+                                           params.interval(),
+                                           params.readingParameters(), rm, js));
+        }
+        else
+        {
+            using testing::_;
+            return EXPECT_CALL(*this, make(yield, _, _, _, _, _, _, rm, js));
+        }
+    }
+
+    auto& expectMake(
+        std::optional<std::reference_wrapper<const ReportParams>> paramsRef,
+        const testing::Matcher<interfaces::ReportManager&>& rm,
+        const testing::Matcher<interfaces::JsonStorage&>& js,
+        const testing::Matcher<std::vector<LabeledMetricParameters>>& lrp)
+    {
+        if (paramsRef)
+        {
+            const ReportParams& params = *paramsRef;
+            return EXPECT_CALL(*this,
+                               make(params.reportName(), params.reportingType(),
+                                    params.emitReadingUpdate(),
+                                    params.logToMetricReportCollection(),
+                                    params.interval(),
+                                    params.readingParameters(), rm, js, lrp));
+        }
+        else
+        {
+            using testing::_;
+            return EXPECT_CALL(*this, make(_, _, _, _, _, _, rm, js, lrp));
+        }
+    }
 };
