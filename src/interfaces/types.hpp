7013aea2da18be3d1bd08a386bd59c7a4067a026
Wludzik, Jozef <jozef.wludzik@intel.com>
2020-12-21 14:42:41 +0100

    Add TriggerManager and Trigger objects
    Implemented initial version of Trigger and TriggerManager DBus
    interfaces. Now DBus user is able to add new Trigger and delete it.
    There is no support for other functionality added.
    
    Tested:
     - Built using yocto dependencies with success
     - Unit tests passed
     - Verified manually if Trigger and TriggerManager works as
       expected
    
    Change-Id: Ie68463526ecccc3be67cc7bfaaf9a9aa7326dee6
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>

--- src/interfaces/types.hpp
+++ src/interfaces/types.hpp
@@ -27,3 +27,44 @@ using LabeledMetricParameters =
 using Readings = std::tuple<
     uint64_t,
     std::vector<std::tuple<std::string, std::string, double, uint64_t>>>;
+
+enum DiscreteConditionType : int
+{
+    changed = 0,
+    specified
+};
+
+enum DiscreteSeverityType : int
+{
+    ok = 0,
+    warning,
+    critical
+};
+
+using DiscreteThresholdParam =
+    std::tuple<std::string, std::underlying_type_t<DiscreteSeverityType>,
+               std::variant<double>, uint64_t>;
+using DiscreteThresholdParams =
+    std::tuple<std::underlying_type_t<DiscreteConditionType>,
+               std::vector<DiscreteThresholdParam>>;
+
+enum NumericThresholdType : int
+{
+    lowerCritical = 0,
+    lowerWarning,
+    upperWarning,
+    upperCritical
+};
+
+enum NumericActivationType : int
+{
+    either = 0,
+    decreasing,
+    increasing
+};
+
+using NumericThresholdParam =
+    std::tuple<std::underlying_type_t<NumericThresholdType>, uint64_t,
+               std::underlying_type_t<NumericActivationType>, double>;
+using TriggerThresholdParams =
+    std::variant<DiscreteThresholdParams, std::vector<NumericThresholdParam>>;
