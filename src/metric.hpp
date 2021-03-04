380fe0f88c9bb7c48ade10a2653375cc70d3bd3f
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2021-03-04 09:49:27 +0000

    added support for Collection(Function, TimeScope, Duration)
    new supported operations: min,max,sum,avg
    new supported time scopes: interval,startup
    
    added unit test to verify that each collection function returns correct
    timestamp and value
    
    Tested:
    - POST/GET on telemetry features in bmcweb, no regression detected
    
    Change-Id: Ib364c433915e07fd7a102f00109525362c40ab8a
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

--- src/metric.hpp
+++ src/metric.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "interfaces/clock.hpp"
 #include "interfaces/metric.hpp"
 #include "interfaces/sensor.hpp"
 #include "interfaces/sensor_listener.hpp"
@@ -11,22 +12,35 @@ class Metric :
 {
   public:
     Metric(Sensors sensors, OperationType operationType, std::string id,
-           std::string metadata, CollectionTimeScope, CollectionDuration);
+           std::string metadata, CollectionTimeScope, CollectionDuration,
+           std::unique_ptr<interfaces::Clock>);
+    ~Metric();
 
     void initialize() override;
-    const std::vector<MetricValue>& getReadings() const override;
+    std::vector<MetricValue> getReadings() const override;
     void sensorUpdated(interfaces::Sensor&, uint64_t) override;
     void sensorUpdated(interfaces::Sensor&, uint64_t, double value) override;
     LabeledMetricParameters dumpConfiguration() const override;
 
   private:
-    MetricValue& findMetric(interfaces::Sensor&);
+    class CollectionData;
+    class DataPoint;
+    class DataInterval;
+    class DataStartup;
+
+    static std::vector<std::unique_ptr<CollectionData>>
+        makeCollectionData(size_t size, OperationType, CollectionTimeScope,
+                           CollectionDuration);
+
+    CollectionData& findAssociatedData(interfaces::Sensor& notifier);
 
     std::string id;
     std::string metadata;
     std::vector<MetricValue> readings;
     Sensors sensors;
     OperationType operationType;
-    CollectionTimeScope timeScope;
+    CollectionTimeScope collectionTimeScope;
     CollectionDuration collectionDuration;
+    std::vector<std::unique_ptr<CollectionData>> collectionAlgorithms;
+    std::unique_ptr<interfaces::Clock> clock;
 };
