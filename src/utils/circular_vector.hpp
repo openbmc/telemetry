8787ee171191b185db96de51bd82a2448536ed24
Szymon Dompke <szymon.dompke@intel.com>
2021-09-20 15:32:04 +0200

    Add support for AppendLimit and ReportUpdates
    Added 2 new properties for Report interface: AppendLimit and
    ReportUpdates. They were also added as arguments to the future version
    of AddReport method of ReportManager.
    
    ReportUpdates property defines the report update behavior:
    - Overwrite: Each report update overrides previous "Readings" property.
      "AppendLimit" set by user is not respected - "Readings" property size
      is equal to count of all sensor across all metrics defined in report.
    - AppendWrapsWhenFull: New readings are appended until limit specified
      by "AppendLimit" is reached. Then oldest readings are overwritten by
      new ones.
    - AppendStopsWhenFull: New readings are appended until limit specified
      by "AppendLimit" is reached. Then updates are stopped.
    - NewReport: not supported yet and will be implemented in the future.
    
    Tested:
    - Both new properties can be accessed from dbus.
    - Both properties are reflected in Readings property.
    - Old AddReport method is working as before the change.
    - UTs are passing.
    
    Signed-off-by: Szymon Dompke <szymon.dompke@intel.com>
    Change-Id: I8a18f7e68215f0f6e5c403b533d2c4ff479df69e

---
+++ src/utils/circular_vector.hpp
@@ -0,0 +1,61 @@
+#include <vector>
+
+template <class T>
+class CircularVector
+{
+  public:
+    CircularVector(size_t maxSizeIn) : maxSize(maxSizeIn)
+    {
+        clear();
+    }
+
+    template <class... Args>
+    void emplace(Args&&... args)
+    {
+        if (maxSize == 0)
+        {
+            return;
+        }
+        if (vec.size() == maxSize)
+        {
+            vec.at(idx) = T(std::forward<Args>(args)...);
+        }
+        else
+        {
+            vec.emplace_back(std::forward<Args>(args)...);
+        }
+        idx = (idx + 1 == maxSize ? 0 : idx + 1);
+    }
+
+    void clear()
+    {
+        vec.clear();
+        idx = 0;
+    }
+
+    void clearAndResize(size_t newMaxSize)
+    {
+        clear();
+        maxSize = newMaxSize;
+    }
+
+    bool isFull() const
+    {
+        return vec.size() == maxSize;
+    }
+
+    typename std::vector<T>::const_iterator begin() const
+    {
+        return vec.begin();
+    }
+
+    typename std::vector<T>::const_iterator end() const
+    {
+        return vec.end();
+    }
+
+  private:
+    size_t maxSize;
+    size_t idx = 0;
+    std::vector<T> vec;
+};
