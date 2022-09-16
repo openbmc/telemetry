50825c9e9d1aeba1dc54e1700bbcf5eadae468ee
Szymon Dompke <szymon.dompke@intel.com>
2022-09-16 15:23:30 +0200

    Fix clang errors
    - structs used in emplace_back now have defined constructor
    - removed dead code from ensure
    
    Testing done:
    - CI tests are passing
    - UTs are passing
    
    Signed-off-by: Szymon Dompke <szymon.dompke@intel.com>
    Change-Id: I3f56a3796e45d608c7a5d3da1f8c2d674d62f567

--- src/types/error_message.hpp
+++ src/types/error_message.hpp
@@ -8,4 +8,8 @@ struct ErrorMessage
 {
     ErrorType error;
     std::string arg0;
+
+    ErrorMessage(ErrorType errorIn, std::string_view arg0In) :
+        error(errorIn), arg0(arg0In)
+    {}
 };
