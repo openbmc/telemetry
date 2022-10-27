93f4ad105bff44b5fa304c683ba2d833f2909f40
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2022-10-27 07:16:08 +0000

    Removed FutureVersion from API
    Instead of using FutureVersion API currently used version is updated.
    This change needs to be bumped together with [1].
    
    Tested:
    - After chaging bmcweb to use new API old and new features are working
      as expected
    
    [1]: https://gerrit.openbmc.org/c/openbmc/bmcweb/+/44270
    
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>
    Change-Id: I5930a466a370f268d68f575a4a3db5ee9655e574

--- src/report.hpp
+++ src/report.hpp
@@ -117,7 +117,6 @@ class Report : public interfaces::Report, public interfaces::MetricListener
     ReportingType reportingType;
     Milliseconds interval;
     std::unordered_set<ReportAction> reportActions;
-    ReadingParametersPastVersion readingParametersPastVersion;
     ReadingParameters readingParameters;
     bool persistency = false;
     uint64_t metricCount;
