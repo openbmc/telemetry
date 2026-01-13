7aeab3d368c69664e2f7214c759e310d9736e53d
Alexander Hansen <alexander.hansen@9elements.com>
2026-01-13 07:23:07 +0100

    dbus_mapper: use PDI constants
    Tested: Inspection only.
    
    Change-Id: Iefeb1f8a1e4a95b985ecae73d2e8fa50f2f9f674
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- src/utils/dbus_mapper.hpp
+++ src/utils/dbus_mapper.hpp
@@ -2,6 +2,8 @@
 
 #include <boost/asio/spawn.hpp>
 #include <sdbusplus/asio/object_server.hpp>
+#include <xyz/openbmc_project/ObjectMapper/common.hpp>
+#include <xyz/openbmc_project/Sensor/Value/common.hpp>
 
 #include <array>
 #include <string>
@@ -16,9 +18,11 @@ using ServiceName = std::string;
 using Ifaces = std::vector<std::string>;
 using SensorIfaces = std::vector<std::pair<ServiceName, Ifaces>>;
 using SensorTree = std::pair<SensorPath, SensorIfaces>;
+using ObjectMapper = sdbusplus::common::xyz::openbmc_project::ObjectMapper;
+using SensorValue = sdbusplus::common::xyz::openbmc_project::sensor::Value;
 
 constexpr std::array<const char*, 1> sensorInterfaces = {
-    "xyz.openbmc_project.Sensor.Value"};
+    SensorValue::interface};
 
 inline std::vector<SensorTree> getSubTreeSensors(
     boost::asio::yield_context& yield,
@@ -27,10 +31,9 @@ inline std::vector<SensorTree> getSubTreeSensors(
     boost::system::error_code ec;
 
     auto tree = bus->yield_method_call<std::vector<SensorTree>>(
-        yield, ec, "xyz.openbmc_project.ObjectMapper",
-        "/xyz/openbmc_project/object_mapper",
-        "xyz.openbmc_project.ObjectMapper", "GetSubTree",
-        "/xyz/openbmc_project/sensors", 2, sensorInterfaces);
+        yield, ec, ObjectMapper::default_service, ObjectMapper::instance_path,
+        ObjectMapper::interface, ObjectMapper::method_names::get_sub_tree,
+        SensorValue::namespace_path::value, 2, sensorInterfaces);
     if (ec)
     {
         throw std::runtime_error("Failed to query ObjectMapper!");
@@ -41,11 +44,10 @@ inline std::vector<SensorTree> getSubTreeSensors(
 inline std::vector<SensorTree> getSubTreeSensors(
     const std::shared_ptr<sdbusplus::asio::connection>& bus)
 {
-    auto method_call =
-        bus->new_method_call("xyz.openbmc_project.ObjectMapper",
-                             "/xyz/openbmc_project/object_mapper",
-                             "xyz.openbmc_project.ObjectMapper", "GetSubTree");
-    method_call.append("/xyz/openbmc_project/sensors/", 2, sensorInterfaces);
+    auto method_call = bus->new_method_call(
+        ObjectMapper::default_service, ObjectMapper::instance_path,
+        ObjectMapper::interface, ObjectMapper::method_names::get_sub_tree);
+    method_call.append(SensorValue::namespace_path::value, 2, sensorInterfaces);
     auto reply = bus->call(method_call);
 
     auto tree = reply.unpack<std::vector<SensorTree>>();
